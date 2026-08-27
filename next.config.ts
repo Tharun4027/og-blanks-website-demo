f05cce851a7149aca58727e68343863e87f4c897
Tharun KL <tharunkumar290312@gmail.com>
2026-08-27 22:32:20 +0530

    Fix GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-
   basePath: "/og-blanks-website-demo",
-
   images: {
     unoptimized: true,
   },
-
   trailingSlash: true,
 };
 
